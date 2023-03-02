366d753b7eaccb58124447f0941259efcaca4225
NI-KI54 <nickmasih575@gmail.com>
2023-03-02 17:03:36 +0530

    updated main.js

--- main.js
+++ main.js
@@ -1,11 +1,25 @@
 const btnEl = document.getElementById("btn");
 const emojiNameEl = document.getElementById("emoji-name");
 
+const emoji = [];
 
+async function getEmoji() {
+    let response = await fetch("https://emoji-api.com/emojis?access_key=ca50f9da4bdef4b842f31ab6d488aedf9490f4b6")
 
+    data = await response.json();
 
+    for (let i = 0; i < 1500; i++) {
+        emoji.push({
+            emojiName: data[i].character,
+            emojiCode: data[i].unicodeName,
+        });
+    }
+}
 
+getEmoji();
 
 btnEl.addEventListener("click", () => {
-    console.log("clicked");
+    const randomNum = Math.floor(Math.random() * emoji.length);
+    btnEl.innerText = emoji[randomNum].emojiName;
+    emojiNameEl.innerText = emoji[randomNum].emojiCode;
 })
